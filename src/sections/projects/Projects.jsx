337623b1a848f0f36e2fea2dad553742552a77a6
Estefanía Yépiz Sandoval <EstefaniaYepiz@gmail.com>
2025-03-13 10:39:52 -0700

    Updated projects
    Added 2 projects to the page

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -1,5 +1,7 @@
 import styles from "./ProjectsStyles.module.css";
 import login from "../../assets/login.png";
+import portfolio from "../../assets/portfolio.png";
+import todo from "../../assets/todo.png";
 import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
@@ -12,6 +14,18 @@ function Projects() {
 					link="https://github.com/EstefaniaYepiz/Log-in_Sign-up_form/tree/main/log-in_sign-up_form"
 					h3="Login Form"
 					p="Login/Sig Up form"
+				/>
+				<ProjectCard
+					src={portfolio}
+					link="https://github.com/EstefaniaYepiz/Estefania-Yepiz"
+					h3="Portfolio"
+					p="This portfolio"
+				/>
+				<ProjectCard
+					src={todo}
+					link="https://github.com/EstefaniaYepiz/Todo-List#"
+					h3="Todo List"
+					p="Simple todo list"
 				/>
 			</div>
 		</section>
